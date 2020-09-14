36822695f73dd3bcd634bf8b44c4e782cee743b8
helgaParis <helga@artistessansfrontieres.fr>
2020-09-14 15:52:55 +0200

    style Link

--- components/layout.js
+++ components/layout.js
@@ -75,7 +75,7 @@ export default function Layout({ children}) {
        
       </header>
 
-      <main className={styles.main}>{children}</main>
+      <main>{children}</main>
       <footer className={styles.footer}>
           <a 
               href="https://artistessansfrontieres.fr"
