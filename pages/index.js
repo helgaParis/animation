587c6ff3c0fd4fc5ae92fd1039e1bb9ce483f9ec
helgaParis <helga@artistessansfrontieres.fr>
2020-09-28 15:05:32 +0200

    added email link

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 
 import styles from '../styles/Home.module.css'
 import stylesPlan from '../styles/plan.module.css'
+const email='conseil14@asf-web.fr'
 
 export default function Home() {
   return (
@@ -19,7 +20,9 @@ export default function Home() {
            <h2>Plenière le 6 octobre 2020 à 19h</h2>
             <p>Cette page est temporaire. Vous pouvez nous envoyer vos propositions concernant l'ordre du jour de la Pleinière et 
                 autres voeux via ce site : </p>
-            <p>Par email: à partir du 1 octobre 2020</p>
+            <p>Envoyer des {" "}
+            <a href={`mailto:${email}?subject=Plenière du Conseil du Quartier JM PO&body= Mes Propositions :`}>propositions par email</a>
+            </p>
             <p> Via un formulaire: quelqus jours avant la Plenière</p>
             <h2>Lien vers la réunion zoom</h2>   
             <p>Vous le trouverez sur votre invitation. Il apparaitra également sur cette page un peu avant la réunion</p>
