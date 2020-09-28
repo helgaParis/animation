4dd97ecac6863ca4c4e022828aa50bb188142cbc
helgaParis <helga@artistessansfrontieres.fr>
2020-09-28 18:16:34 +0200

    list style added

--- pages/index.js
+++ pages/index.js
@@ -20,11 +20,15 @@ export default function Home() {
            <h2>Plénière le 6 octobre 2020 à 19h</h2>
             <p>Vous pouvez nous envoyer vos propositions concernant l'ordre du jour de la plénière et 
                 autres voeux via ce site : </p>
-            <p>Envoyer des {" "}
-            <a href={`mailto:${email}?subject=Plenière du Conseil du Quartier JM PO&body= Mes Propositions :`}>propositions par email</a>
-            </p>
-      
-            <p> Via un formulaire: quelques jours avant la plénière</p>
+                <ul>
+                    <li> <p>Envoyer des {" "}
+                    <a href={`mailto:${email}?subject=Plenière du Conseil du Quartier JM PO&body= Mes Propositions :`}>propositions par email</a>
+                    </p>
+                    </li>
+                    <li> <p> Via un formulaire: quelques jours avant la plénière</p></li>
+                </ul>
+           
+           
             <p>Attention ! Cette page a été conçue pour recueillir vos propositions, mais elle est temporaire, elle sera fermée à la suite de la plénière.  </p>
             <h2>Lien vers la réunion zoom du 6 octobre à 19h</h2>   
             <p>Vous trouverez le lien sur votre invitation. Il apparaitra également sur cette page un peu avant la réunion</p>
