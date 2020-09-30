654c624752a6f6bdb0c5f0a666f5a0b1834bdc99
helgaParis <helga@artistessansfrontieres.fr>
2020-09-30 17:15:22 +0200

    lang element added

--- pages/_documents.js
+++ pages/_documents.js
@@ -1,3 +1,30 @@
+//Code for Next 9.5.3 app
+import Document, { Head, Main, NextScript } from 'next/document'
+import flush from 'styled-jsx/server'
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+   const { html, head, errorHtml, chunks } = renderPage()
+   const styles = flush()
+   return { html, head, errorHtml, chunks, styles }
+  }
+  render() {
+    return (
+      <html lang="fr">
+        <Head></Head>
+        <body> 
+          {this.props.customValue}
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
+
+
+
+/* This is the code for next 9.5.4 canary app, published in Sept 2020
+
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
@@ -8,7 +35,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="fr">
+        <Html lang="fr">
         <Head />
         <body>
           <Main />
@@ -20,3 +47,4 @@ class MyDocument extends Document {
 }
 
 export default MyDocument
+*/
