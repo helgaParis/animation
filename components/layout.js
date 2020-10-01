c4bc15da3b54254136959c854b6a15623b84fb0f
helgaParis <helga@artistessansfrontieres.fr>
2020-10-01 18:57:22 +0000

    viewport added

--- components/layout.js
+++ components/layout.js
@@ -9,7 +9,11 @@ export const siteTitle = 'Conseil de quartier JMPO'
 export default function Layout({ children}) {
     return (
         <div className={styles.container}>
-            <Head>          
+            <Head>         
+                <meta 
+                name="viewport" 
+                content="width=device-width, initial-scale=1.0"
+                /> 
                 <link rel="icon" href="/favicon.ico" />
                 <meta
                     name="description"
