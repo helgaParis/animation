a2d61eaa0587e541492fee01fca3a38de4c0a075
helgaParis <helga@artistessansfrontieres.fr>
2020-10-11 18:18:23 +0000

    test serverside props (will not be used on site)

---
+++ pages/actualites.js
@@ -0,0 +1,38 @@
+//server side props, active calls to db, without layout, would be slow
+
+import { connectToDatabase } from "../util/mongodb";
+
+export default function Actualites({ articles }) {
+  return (
+	<div>
+	  <h1>Les nouvelles du quartier et du conseil du quartier</h1>
+	  <ul>
+		{articles.map((article) => (
+		  <li key={article.id}>
+			<h2>{article.title}</h2>
+			<h3>{article.text}</h3>
+			<p>{article.author}</p>
+			<p>{article.date}</p>
+		  </li>
+		))}
+	  </ul>
+	</div>
+  );
+}
+
+export async function getServerSideProps() {
+  const { db } = await connectToDatabase();
+
+  const articles = await db
+	.collection("articles")
+	.find({blog:true})
+	.sort({ date: -1 })
+	.limit(20)
+	.toArray();
+
+  return {
+	props: {
+	  articles: JSON.parse(JSON.stringify(articles)),
+	},
+  };
+}
