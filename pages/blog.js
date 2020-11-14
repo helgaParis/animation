cc66851e90102f7de4087e998c9f0d555f9b6af3
helgaParis <helga@artistessansfrontieres.fr>
2020-11-14 15:38:33 +0000

    added a map

--- pages/blog.js
+++ pages/blog.js
@@ -1,4 +1,4 @@
-//static props, up to last100 articles included into build
+//static props, up to last100 articles included into build 
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import styles from '../styles/Home.module.css'
@@ -15,6 +15,7 @@ export default function Blogposts({articles}) {
         
         <h1 className={styles.title}>Les Actualités</h1>
         <p>Retrouvez sur cette page les dernières nouvelles du conseil de quartier Jean Moulin - Porte d’Orléans et de l’ensemble de ses commissions.</p>
+        <iframe width="90%" height="300px" frameborder="0" allowfullscreen src="//umap.openstreetmap.fr/fr/map/les-petits-commerces-du-14e-en-temps-de-confinemen_518343?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=null&allowEdit=false&moreControl=true&searchControl=true&tilelayersControl=null&embedControl=true&datalayersControl=true&onLoadPanel=caption&captionBar=false&locateControl=true&fullscreenControl=false#15/48.8292/2.3344"></iframe><p><a href="//umap.openstreetmap.fr/fr/map/les-petits-commerces-du-14e-en-temps-de-confinemen_518343" rel="noopener noreferrer">Voir en plein écran</a></p>
         <ul>
           {articles.map((article) => (
             <li key={article.id}>
@@ -43,7 +44,7 @@ export default function Blogposts({articles}) {
       props: {
         articles: JSON.parse(JSON.stringify(articles)),
       },
-      // - the revalidate command adds regeneration to the static page 
-      revalidate: 1, // In seconds, every n seconds max
+      // - the revalidate command adds regeneration to the static page  
+      revalidate: 1, // In seconds, every n seconds max - it's actually much slower, a few minutes to see change
     };
   }
