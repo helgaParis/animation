53fbfc7a148e8819e4c0e39f449e16a74e0e79ac
helgaParis <helga@artistessansfrontieres.fr>
2020-11-14 18:01:04 +0000

    prettier date format and link target

--- pages/blog.js
+++ pages/blog.js
@@ -16,13 +16,13 @@ export default function Blogposts({articles}) {
         
         <h1 className={styles.title}>Les Actualités</h1>
         <p>Retrouvez sur cette page les dernières nouvelles du conseil de quartier Jean Moulin - Porte d’Orléans et de l’ensemble de ses commissions.</p>     
-        <iframe width="90%" height="350px" frameborder="0" allowfullscreen src="//umap.openstreetmap.fr/fr/map/les-petits-commerces-du-14e-en-temps-de-confinemen_518343?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=null&allowEdit=false&moreControl=false&searchControl=true&tilelayersControl=null&embedControl=true&datalayersControl=true&onLoadPanel=none&captionBar=false&locateControl=true&fullscreenControl=false&editinosmControl=false#15/48.8292/2.3344"></iframe><p><a href="//umap.openstreetmap.fr/fr/map/les-petits-commerces-du-14e-en-temps-de-confinemen_518343" rel="noopener noreferrer">Voir en plein écran</a></p>
+        <iframe width="90%" height="350px" frameborder="0" allowfullscreen src="//umap.openstreetmap.fr/fr/map/les-petits-commerces-du-14e-en-temps-de-confinemen_518343?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=null&allowEdit=false&moreControl=false&searchControl=true&tilelayersControl=null&embedControl=true&datalayersControl=true&onLoadPanel=none&captionBar=false&locateControl=true&fullscreenControl=false&editinosmControl=false#15/48.8292/2.3344"></iframe><p><a href="//umap.openstreetmap.fr/fr/map/les-petits-commerces-du-14e-en-temps-de-confinemen_518343" rel="noopener noreferrer"target="_blank">Voir en plein écran</a></p>
         <ul>
           {articles.map((article) => (
             <li key={article.id}>
             <h2  className={styles.blogtitle} >{article.title}</h2>
             <p  className={styles.blogtext}>{article.text}</p>
-            <p  className={styles.bloginfos}>{article.author}, le {article.date}</p>
+            <p  className={styles.bloginfos}>{article.author}, le {article.date.slice(0,10)} à {article.date.slice(11,16)} h</p>
             </li>
           ))}
         </ul>
@@ -45,6 +45,7 @@ export default function Blogposts({articles}) {
       props: {
         articles: JSON.parse(JSON.stringify(articles)),
       },
+      
       // - the revalidate command adds regeneration to the static page  
       revalidate: 1, // In seconds, every n seconds max - it's actually much slower, a few minutes to see change
     };
