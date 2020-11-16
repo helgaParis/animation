4f2b14263dd59e6ae707a8ef981f59afb6f4c8b5
helgaParis <helga@artistessansfrontieres.fr>
2020-11-16 19:18:34 +0000

    corrected a typo

--- pages/blog.js
+++ pages/blog.js
@@ -18,22 +18,22 @@ export default function Blogposts({articles}) {
         <p>Retrouvez sur cette page les dernières nouvelles du conseil de quartier Jean Moulin - Porte d’Orléans et de l’ensemble de ses commissions.</p>     
         <iframe width="90%" height="350px" frameborder="0" allowfullscreen src="//umap.openstreetmap.fr/fr/map/les-petits-commerces-du-14e-en-temps-de-confinemen_518343?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=null&allowEdit=false&moreControl=false&searchControl=true&tilelayersControl=null&embedControl=true&datalayersControl=true&onLoadPanel=none&captionBar=false&locateControl=true&fullscreenControl=false&editinosmControl=false#15/48.8292/2.3344"></iframe><p><a href="//umap.openstreetmap.fr/fr/map/les-petits-commerces-du-14e-en-temps-de-confinemen_518343" rel="noopener noreferrer"target="_blank">Voir en plein écran</a></p>
         <ul>
-          {articles.map((article) => (
-            <li key={article.id}>
-            <h2  className={styles.blogtitle} >{article.title}</h2>
-            <p  className={styles.blogtext}>{article.text}</p>
-            <p>{""}<a
-             href={article.link} 
-            title="Compte Rendu du 26 mai 2020"
-            target="_blank"
-            rel="noopener noreferrer" >
-            {article.link}            
-            </a></p>
+        {articles.map((article) => (
+          <li key={article.id}>
+          <h2  className={styles.blogtitle} >{article.title}</h2>
+          <p  className={styles.blogtext}>{article.text}</p>
+          <p>{""}<a
+           href={article.link} 
+          title={article.link}
+          target="_blank"
+          rel="noopener noreferrer" >
+          {article.link}            
+          </a></p>
 
-            <p  className={styles.bloginfos}>{article.author}, le {article.date.slice(0,10)} à {article.date.slice(11,16)} h</p>
-            </li>
-          ))}
-        </ul>
+          <p  className={styles.bloginfos}>{article.author}, le {article.date.slice(0,10)} à {article.date.slice(11,16)} h</p>
+          </li>
+        ))}
+      </ul>
         </main>
        
       </Layout>
@@ -58,3 +58,8 @@ export default function Blogposts({articles}) {
       revalidate: 1, // In seconds, every n seconds max - it's actually much slower, a few minutes to see change
     };
   }
+  
+  
+  
+  
+ 
