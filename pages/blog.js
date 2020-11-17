b349189032d79fa1f442ca038d4f1bdf71dd620b
helgaParis <helga@artistessansfrontieres.fr>
2020-11-17 17:26:01 +0000

    added conditions to hide empty lines and to map

--- pages/blog.js
+++ pages/blog.js
@@ -12,7 +12,7 @@ export default function Blogposts({articles}) {
           <title>Actualités - {siteTitle}</title>
           <meta
           name="keywords"
-          content="Actualités du conseil de quartier, news, Porte d'Orléans, Jean Moulin, Alésia, commerces ouvertes, carte, plan, confinement, contribuer, vie de quartier"
+          content="Actualités du conseil de quartier, news, Porte d'Orléans, Jean Moulin, Alésia, Paris 14e, commerces ouvertes, carte, plan, confinement, contribuer, vie de quartier"
       />
         </Head>
         <main className={styles.main}>
@@ -20,24 +20,26 @@ export default function Blogposts({articles}) {
         
         <h1 className={styles.title}>Les Actualités</h1>
         <p>Retrouvez sur cette page les dernières nouvelles du conseil de quartier Jean Moulin - Porte d’Orléans et de l’ensemble de ses commissions.</p>     
-        <iframe width="90%" height="350px" frameborder="0" allowfullscreen src="//umap.openstreetmap.fr/fr/map/les-petits-commerces-du-14e-en-temps-de-confinemen_518343?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=null&allowEdit=false&moreControl=false&searchControl=true&tilelayersControl=null&embedControl=true&datalayersControl=true&onLoadPanel=none&captionBar=false&locateControl=true&fullscreenControl=false&editinosmControl=false#15/48.8292/2.3344"></iframe><p><a href="//umap.openstreetmap.fr/fr/map/les-petits-commerces-du-14e-en-temps-de-confinemen_518343" rel="noopener noreferrer"target="_blank">Voir en plein écran</a></p>
+        <iframe width="90%" height="350px" frameBorder="0"  src="//umap.openstreetmap.fr/fr/map/les-petits-commerces-du-14e-en-temps-de-confinemen_518343?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=null&allowEdit=false&moreControl=false&searchControl=true&tilelayersControl=false&embedControl=true&datalayersControl=true&onLoadPanel=none&captionBar=false&locateControl=true&fullscreenControl=false&editinosmControl=false#15/48.8292/2.3344"></iframe><p><a href="//umap.openstreetmap.fr/fr/map/les-petits-commerces-du-14e-en-temps-de-confinemen_518343" rel="noopener noreferrer"target="_blank">Voir en plein écran</a></p>
         <ul>
         {articles.map((article) => (
           <li key={article.id}>
           <h2  className={styles.blogtitle} >{article.title}</h2>
           <p  className={styles.blogtext}>{article.text}</p>
-          <p>
-          <img src={article.image1} className={styles.blogimage} alt={article.alt1} ></img>
-          <img src={article.image2} className={styles.blogimage} alt={article.alt2} ></img>
-          </p>
-          <p>{""}<a
+          {article.image1 && <img src={article.image1} className={styles.blogimage} alt={article.alt1} ></img>}
+          {article.image2 && <img src={article.image2} className={styles.blogimage} alt={article.alt2} ></img>}
+          {article.alt1 && <p className={styles.blogimgtext}>{article.alt1} 
+          {article.alt2 && <span> - {article.alt2} </span>}
+          </p>  }
+          {article.link && <p>{""}
+          <a
            href={article.link} 
           title={article.link}
           target="_blank"
           rel="noopener noreferrer" >
           {article.link}            
-          </a></p>
-
+          </a>
+          </p>}
           <p  className={styles.bloginfos}>{article.author}, le {article.date.slice(0,10)} à {article.date.slice(11,16)} h</p>
           </li>
         ))}
