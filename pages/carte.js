6f56c2237b7d94420e40da41fc82d02c944446e2
helgaParis <helga@artistessansfrontieres.fr>
2021-03-05 19:51:40 +0000

    removed plan from blog

--- pages/carte.js
+++ pages/carte.js
@@ -12,7 +12,7 @@ export default function Carte() {
        
             <h1 className={styles.title} >Commerces ouvertes pendant le confinement en 2020 </h1>
           
-           <iframe width="90%" height="350px" frameBorder="0"  title="carteCommercesOuvertes" src="//umap.openstreetmap.fr/fr/map/les-petits-commerces-du-14e-en-temps-de-confinemen_518343?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=null&allowEdit=false&moreControl=false&searchControl=true&tilelayersControl=false&embedControl=true&datalayersControl=true&onLoadPanel=none&captionBar=false&locateControl=true&fullscreenControl=false&editinosmControl=false#15/48.8292/2.3344"></iframe><p><a href="//umap.openstreetmap.fr/fr/map/les-petits-commerces-du-14e-en-temps-de-confinemen_518343" rel="noopener noreferrer"target="_blank">Voir en plein écran</a></p>
+           <iframe width="800px" height="600px" frameBorder="0"  title="carteCommercesOuvertes" src="//umap.openstreetmap.fr/fr/map/les-petits-commerces-du-14e-en-temps-de-confinemen_518343?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=null&allowEdit=false&moreControl=false&searchControl=true&tilelayersControl=false&embedControl=true&datalayersControl=true&onLoadPanel=none&captionBar=false&locateControl=true&fullscreenControl=false&editinosmControl=false#15/48.8292/2.3344"></iframe><p><a href="//umap.openstreetmap.fr/fr/map/les-petits-commerces-du-14e-en-temps-de-confinemen_518343" rel="noopener noreferrer"target="_blank">Voir en plein écran</a></p>
         
         
   
