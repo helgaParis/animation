0d4eb709e90c80677cca6694032c8b15c7578214
helgaParis <helga@artistessansfrontieres.fr>
2021-04-14 13:09:02 +0000

    added db connexion to home

--- pages/blog.js
+++ pages/blog.js
@@ -20,7 +20,7 @@ export default function Blog({articles}) {
         
         <h1 className={styles.title}>Les Actualités</h1>
         <p className={styles.subsubtitle}>Retrouvez sur cette page les dernières nouvelles du conseil de quartier Jean Moulin - Porte d’Orléans et de l’ensemble de ses commissions.</p>     
-        <ul>
+        <ul >
         {articles.map((article) => (
           <li key={article.id}>
           {article.title && <h2  className={styles.blogtitle} >{article.title}</h2>}
