80cc7411c196f5e35e54b826ee3f9269f2717104
helgaParis <helga@artistessansfrontieres.fr>
2021-04-19 16:59:21 +0000

    upgrade to next 10, error page

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,9 @@
 // pages/404.js
 import Layout, { siteTitle } from '../components/layout'
 import Head from 'next/head'
+import Image from "next/image";
 import styles from '../styles/Home.module.css'
+import styleerreur from "../styles/erreur.module.css";
 
 
 export default function Custom404() {
@@ -11,9 +13,20 @@ export default function Custom404() {
           <title>Error 404 site CQ</title>
         </Head>
         <main className={styles.main}>
-        
-        <h1 className={styles.title}>404 - Page introuvable</h1>
+    <div className={styleerreur.degrade}>
+    <h1 className={styles.title}>404 - Page introuvable</h1>
     <p>Le serveur n'a pas trovué la page demandée</p>
+    <div className={styleerreur.errimage}>
+      <Image
+        src="/404.jpg"
+        alt="illustration d'un chemin avec une grenouille un panneau indiquant erreur 404"
+        width={500}
+        height={660}
+        loading="lazy"
+      />
+    </div>
+    </div>
+
     </main>
     </Layout>
     )
