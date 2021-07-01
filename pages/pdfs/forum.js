093355919dacca32eff862dc7f297026c39c3222
helgaParis <helga@artistessansfrontieres.fr>
2021-07-01 10:04:47 +0000

    test affichage pdf

---
+++ pages/pdfs/forum.js
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../../components/layout'
+import styles from '../../styles/Home.module.css'
+
+export default function ForumUrbain() {
+    return (
+        <Layout >
+        <Head>
+          <title>Forum Urbain - {siteTitle}</title>
+        </Head>
+        <main className={styles.main}>
+       
+            <h1 className={styles.title} >Forum Urbain du 6 juin 2021 </h1>
+          <div className={styles.encadreiframe}>
+            
+           <iframe 
+           width = "999px" 
+           height="85vH" 
+           frameBorder="0" 
+          title="Forum Urbain du 6 juin 2021" 
+          src="/forum-urbain-060621.pdf"
+           >
+           </iframe>
+        
+        </div>
+  
+      </main>
+      </Layout>
+    )
+  }
