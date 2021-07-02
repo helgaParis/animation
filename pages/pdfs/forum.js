50f0245404840633e3a497e8266d2d4a3794fa9f
helgaParis <helga@artistessansfrontieres.fr>
2021-07-02 09:49:53 +0000

    css iframe

--- pages/pdfs/forum.js
+++ pages/pdfs/forum.js
@@ -8,15 +8,12 @@ export default function ForumUrbain() {
         <Head>
           <title>Forum Urbain - {siteTitle}</title>
         </Head>
-        <main className={styles.main}>
-       
+      
             <h1 className={styles.title} >Forum Urbain du 6 juin 2021 </h1>
           <div className={styles.encadreiframe}>
             
            <iframe 
-           width = "999px" 
-           height="800px" 
-           frameBorder="0" 
+          className={styles.iframe }
           title="Forum Urbain du 6 juin 2021" 
           src="/forum-urbain-060621.pdf"
            >
@@ -24,7 +21,7 @@ export default function ForumUrbain() {
         
         </div>
   
-      </main>
+      
       </Layout>
     )
   }
