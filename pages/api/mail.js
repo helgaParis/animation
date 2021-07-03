f856be8ffcb627486a90affc6c51a39e250fca8c
helgaParis <helga@artistessansfrontieres.fr>
2021-07-03 21:11:50 +0000

    added contact form

---
+++ pages/api/mail.js
@@ -0,0 +1,60 @@
+
+// installer dotenv et nodemailer
+// definir le email mp dans .env local et sur le serveur, dans le cloud
+
+export default function handler (req, res) {
+  require('dotenv').config()
+
+  //si un autre méthode que post (notre formulaire) est employé, le handler refuse de traiter la requette. Pour tester, débloquer la reponse json
+  if (req.method != "POST") {
+	return res.status(405)
+	//.json({ message: "Method not allowed" });
+  }
+  
+	let nodemailer = require('nodemailer')
+    const transporter = nodemailer.createTransport({
+		port: 465,
+		host: "smtp.gmail.com",
+		auth: {
+		  user: 'artists.asf@gmail.com',
+		  pass: process.env.mailpassword,
+		},
+		secure: true,
+ 	})
+ 
+  	const mailData = {
+		from: `artists.asf@gmail.com`,
+		to: 'conseil14@asf-web.fr',
+		subject: `Message via le formulaire de contact du site `,
+		text: req.body.message + " Envoyé de : " + req.body.email,
+		html: `<div style="font-size:1.3em; font-weight:600;">
+			<p>Repondre à cette adresse email :
+			${req.body.email}</p>
+			<p>Nom : ${req.body.lastName} </p>
+			<p>Prénom: ${req.body.firstName}</p>
+			<p>Message :</p>
+			<div style="background-color:bisque; font-size:1.3em">
+			${req.body.message}
+			</div>
+		</div>
+		`
+	}
+	//pour tester ou voir toute le processus, mettre console.log(info) 
+    transporter.sendMail(mailData, function (err, info) {
+		if(err)
+		  console.log(err)
+		else
+		  console.log(info.messageId)
+  		})
+	//il faut que le handler retourne un status 200 au fetcher pour que le formulaire se vide ou autre chose se passe sur la page
+	res.status(200).json({})
+	//il semble avoir un bug - il faut ajouter un json vide (ou un message réel) pour que le handler signale la fin. Alternativement, on peut faire un redirect, ex à une page "Merci pour la contribution"
+	//res.redirect([status,] path)
+
+}
+//Next a un resolver inclus dans le handler, un external resolver peut terminer le processus, mais n'envoie pas de status 200.
+// export const config = {
+// 	  api: {
+// 		externalResolver: true,
+// 	  },
+// 	}
--- pages/contribuer.js
+++ pages/contribuer.js
@@ -1,26 +1,131 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import styles from '../styles/Home.module.css'
-//import React from 'react'
-//import Collection from './api/add-to-db.js'
-const email='conseil14@asf-web.fr'
+import formstyles from '../styles/form.module.css'
+import Link from 'next/link'
+import React from 'react'
 
-export default function Contribuer() {
+export default function Contribue() {
+  const email='conseil14@asf-web.fr' //pour le email-link à la fin
+  {/* Definir une constante dans le state - contactdata- et la façon pour la modifier - setContactdata en utilisant React state et useState 
+    La constante contient le contenue des champs du formulaire*/}
+    const [contactdata, setContactdata] = React.useState({
+      firstName: "",
+      lastName: "",
+      email:"",
+      message:"",
+      // show: true
+      })
+    
+    {/* handle Change lit chaque changement d'n événement, en ce cas le changement en écrivant dans les cases du formulaire 
+        La fonction prend en argument les événements dans les champs ciblés par l'utilisateur 
+        Elle prend ce qu'existe déjà et ajoute chaque signe tapé*/}
+      function handleChange(evt) {
+        const value = evt.target.value;
+        setContactdata({
+          ...contactdata,
+          [evt.target.name]: value
+        });
+      }
+  
+  {/* Au moment de l'envoi: prevent default, le console log peut être decommenté pour controler si tous les champs fonctionnent
+   Le fetcher contacte l'api/mail en post , avec des headers et le contenu du formlaire (la constante dans le state), transformé en text (stringified ) dans le body
+   après le travail de l'api, then, un check, if la reponse est ok (200). Decommenter les console log ou l'alerte avec le status pour vérifier quand cela se passe. Après le 200, le formulaire se vide.
+   Independant de la reponse, le window alert dit merci - on pourrait le mettre avant le reload, quand c'est confirmé par l'api, mais cela peut prendre trop de temps. Le mail arrive souvent avant la reponse 200.
+    */}
+      function handleSubmit(evt) {
+        evt.preventDefault();
+          {/* console.log('ici il devrait avoir le resultat : '+
+           state.firstName + '  ' + state.lastName + '  ' + state.email+ '  ' + state.message); */}
+        fetch("/api/mail", {
+          method: "POST",
+          headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+          body: JSON.stringify(contactdata),
+          }).then((res) => {
+            //console.log("Response received");
+            if (res.status === 200) {
+              //console.log("Response succeeded!");
+              //alert(res.status);
+              //ici c'est sur que le mail a été envoyé, raffraichis la page
+              window.location.reload();
+            }
+      });
+      // ici c'est seulement envoyé à l'api
+      alert("Merci pour votre contribution!");
+    }
 
 
     return (
-        <Layout >
+      <Layout >
         <Head>
-          <title>Contribuer - {siteTitle}</title>
-        </Head>
-        <main className={styles.main}>
-        
-        <h1 className={styles.title}>Participer au travail du Conseil de Quartier</h1>
-        <p>Vous pouvez nous envoyer vos propositons ou vous inscrire sur notre liste de communication</p>
+          <title>{siteTitle}</title>
+        </Head>     
+        <div>   
+        <h1 className={styles.title}>Contacter le Conseil de Quartier</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+              Prénom                                
+              <input
+                className={formstyles.input}
+                type="text"
+                name="firstName"
+                required
+                value={contactdata.firstName}
+                onChange={handleChange}
+              />
+          </label>
+          <label>
+            Nom de famille
+            <input
+             className={formstyles.input}
+              type="text"
+              name="lastName"
+              required
+              value={contactdata.lastName}
+              onChange={handleChange}
+            />
+          </label>
+          <br/>
+          <label>
+            Adresse Email
+            <input
+              className={formstyles.input}
+              type="email"
+              name="email" 
+              required 
+              value={contactdata.email} 
+              onChange={handleChange}
+            />
+          </label>
+          <br/>
+          <label>
+            Ma contribution
+          <br/>
+          <textarea 
+            className={formstyles.text}
+            name="message" 
+            required 
+            value={contactdata.message} 
+            onChange={handleChange}
+           />
+          </label>
+          <br/>
+          <label>
+            <input
+            className={formstyles.submit}
+            type="submit" 
+            value="Envoyer"
+            />
+          </label>
+        </form>
+        </div>
+        <div className={formstyles.extra}>
+        <p>Vous pouvez nous envoyer vos propositons par email également</p>
         <h4><a href={`mailto:${email}?subject=Contributions - Conseil de Quartier JM PO&body=Ma contribution : `}>Email : conseil14@asf-web.fr</a></h4>
-        <p>Si le lien n'ouvre pas votre messagerie, vous pouvez copier l'adresse mail : conseil14@asf-web.fr</p>
-      
-      </main>
-      </Layout>
+        </div>
+              </Layout>
     )
   }
